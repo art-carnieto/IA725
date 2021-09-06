99f727c4c78e53a770071560ea888b8a07f28d6f
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2021-09-06 20:14:17 -0300

    Draw simple 20 faces icosahedron
    Original vertices positions and indices was created by Anisha Gartia and all copy rights are hers. The code can be found on https://github.com/anishagartia/Icosahedron_OpenGL . Wireframe mode was enabled to test the visuals of the triangles and check if they are correct.

--- Icosaedro/main.cpp
+++ Icosaedro/main.cpp
@@ -111,7 +111,7 @@ static void RenderSceneCB()
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, (3*5*4), GL_UNSIGNED_INT, 0);
 
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
@@ -142,16 +142,26 @@ struct Vertex {
 
 static void CreateVertexBuffer()
 {
-    Vertex Vertices[8];
+    const float posX = 0.525731112119133606f;
+    const float posY = 0.0f;
+    const float posZ = 0.850650808352039932f;
 
-    Vertices[0] = Vertex(0.5f, 0.5f, 0.5f);
-    Vertices[1] = Vertex(-0.5f, 0.5f, -0.5f);
-    Vertices[2] = Vertex(-0.5f, 0.5f, 0.5f);
-    Vertices[3] = Vertex(0.5f, -0.5f, -0.5f);
-    Vertices[4] = Vertex(-0.5f, -0.5f, -0.5f);
-    Vertices[5] = Vertex(0.5f, 0.5f, -0.5f);
-    Vertices[6] = Vertex(0.5f, -0.5f, 0.5f);
-    Vertices[7] = Vertex(-0.5f, -0.5f, 0.5f);
+    Vertex Vertices[12];
+
+    Vertices[0] = Vertex(-posX, 0.0f,  posZ);
+    Vertices[1] = Vertex( posX, 0.0f,  posZ);
+    Vertices[2] = Vertex(-posX, 0.0f, -posZ);
+    Vertices[3] = Vertex( posX, 0.0f, -posZ);
+
+    Vertices[4] = Vertex( 0.0f, posZ,  posX);
+    Vertices[5] = Vertex( 0.0f, posZ, -posX);
+    Vertices[6] = Vertex( 0.0f,-posZ,  posX);
+    Vertices[7] = Vertex( 0.0f,-posZ, -posX);
+
+    Vertices[8] =  Vertex( posZ, posX, 0.0f);
+    Vertices[9] =  Vertex(-posZ, posX, 0.0f);
+    Vertices[10] = Vertex( posZ,-posX, 0.0f);
+    Vertices[11] = Vertex(-posZ,-posX, 0.0f);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -161,18 +171,29 @@ static void CreateVertexBuffer()
 static void CreateIndexBuffer()
 {
     unsigned int Indices[] = {
-                              0, 1, 2,
-                              1, 3, 4,
-                              5, 6, 3,
-                              7, 3, 6,
-                              2, 4, 7,
-                              0, 7, 6,
-                              0, 5, 1,
-                              1, 5, 3,
-                              5, 0, 6,
-                              7, 4, 3,
-                              2, 1, 4,
-                              0, 2, 7
+                              0, 4, 1,
+                              0, 9, 4,
+                              9, 5, 4,
+                              4, 5, 8,
+                              4, 8, 1,
+                              
+                              8, 10, 1,
+                              8, 3, 10,
+                              5, 3, 8,
+                              5, 2, 3,
+                              2, 7, 3,
+
+                              7, 10, 3,
+                              7, 6, 10,
+                              7, 11, 6,
+                              11, 0, 6,
+                              0, 1, 6,
+
+                              6, 1, 10,
+                              9, 0, 11,
+                              9, 11, 2,
+                              9, 2, 5,
+                              7, 2, 11
     };
 
     glGenBuffers(1, &IBO);
@@ -298,6 +319,8 @@ int main(int argc, char** argv)
     glEnable(GL_CULL_FACE);
     glFrontFace(GL_CW);
     glCullFace(GL_BACK);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     CreateVertexBuffer();
     CreateIndexBuffer();
