e5ddeae9c4111f7d1208e7a5febe47a8e9dd40b1
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2021-09-07 19:47:41 -0300

    Add icosahedron subdivision calculation
    Original code from tutorial13_youtube has been commented, the vertex and index buffers are not going to be used.
    
    All drawing and subdivision methods has been copied from https://github.com/anishagartia/Icosahedron_OpenGL and is created by Anisha Gartia and all copy rights are hers.
    
    Wireframe mode has been commented.

--- Icosaedro/main.cpp
+++ Icosaedro/main.cpp
@@ -39,6 +39,91 @@ GLuint VBO;
 GLuint IBO;
 GLuint gWVPLocation;
 
+#define NFACE 20
+#define NVERTEX 12
+
+#define X .525731112119133606 
+#define Z .850650808352039932
+
+// These are the 12 vertices for the icosahedron
+static GLfloat vdata[NVERTEX][3] = {
+   {-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},
+   {0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},
+   {Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0}
+};
+
+// These are the 20 faces.  Each of the three entries for each 
+// vertex gives the 3 vertices that make the face.
+static GLint tindices[NFACE][3] = {
+   {0,4,1}, {0,9,4}, {9,5,4}, {4,5,8}, {4,8,1},
+   {8,10,1}, {8,3,10}, {5,3,8}, {5,2,3}, {2,7,3},
+   {7,10,3}, {7,6,10}, {7,11,6}, {11,0,6}, {0,1,6},
+   {6,1,10}, {9,0,11}, {9,11,2}, {9,2,5}, {7,2,11} };
+
+//Helpers
+void normalize(float v[3]) {
+    float d = sqrt((v[0] * v[0]) + (v[1] * v[1]) + (v[2] * v[2]));
+    if (d == 0.0) return;
+    v[0] /= d;
+    v[1] /= d;
+    v[2] /= d;
+}
+
+void drawTriangle(GLfloat* v1, GLfloat* v2, GLfloat* v3)
+{
+    // Draw a triangle with specified vertices 
+    glBegin(GL_TRIANGLES);
+    //  glColor3f(0.0,0.0,0.8);
+    glColor3f(rand() / ((float)RAND_MAX + 1), rand() / ((float)RAND_MAX + 1), rand() / ((float)RAND_MAX + 1));
+    //  glNormal3fv(v1); 
+    //  glNormal3fv(v2); 
+    //  glNormal3fv(v3);
+    glVertex3fv(v1);
+    glVertex3fv(v2);
+    glVertex3fv(v3);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
+    glColor3f(1.0, 1.0, 1.0);
+    //  glNormal3fv(v1); 
+    //  glNormal3fv(v2); 
+    //  glNormal3fv(v3);
+    glVertex3fv(v1);
+    glVertex3fv(v2);
+    glVertex3fv(v3);
+    glEnd();
+}
+
+void subDivide(GLfloat* v1, GLfloat* v2, GLfloat* v3, int depth) {
+    if (depth == 0) {
+        drawTriangle(v1, v2, v3);
+        return;
+    }
+    //midpoint of each edge
+    GLfloat v12[3];
+    GLfloat v23[3];
+    GLfloat v31[3];
+    for (int i = 0; i < 3; i++) {
+        v12[i] = v1[i] + v2[i];
+        v23[i] = v2[i] + v3[i];
+        v31[i] = v3[i] + v1[i];
+    }
+    normalize(v12);
+    normalize(v23);
+    normalize(v31);
+
+    subDivide(v1, v12, v31, depth - 1);
+    subDivide(v2, v23, v12, depth - 1);
+    subDivide(v3, v31, v23, depth - 1);
+    subDivide(v12, v23, v31, depth - 1);
+}
+
+void drawIcosahedron(int maxDepth) {
+    for (int i = 0; i < NFACE; i++) {
+        srand(i);
+        subDivide(&vdata[tindices[i][0]][0], &vdata[tindices[i][1]][0], &vdata[tindices[i][2]][0], maxDepth);
+    }
+}
 
 static void RenderSceneCB()
 {
@@ -100,28 +185,14 @@ static void RenderSceneCB()
 
     glUniformMatrix4fv(gWVPLocation, 1, GL_TRUE, &WVP.m[0][0]);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-
-    // position
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
-
-    // color
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-
-    glDrawElements(GL_TRIANGLES, (3*5*4), GL_UNSIGNED_INT, 0);
-
-    glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
+    drawIcosahedron(3); // draw icosahedron of order 3
 
     glutPostRedisplay();
 
     glutSwapBuffers();
 }
 
-
+/*
 struct Vertex {
     Vector3f pos;
     Vector3f color;
@@ -138,7 +209,6 @@ struct Vertex {
         color = Vector3f(red, green, blue);
     }
 };
-
 
 static void CreateVertexBuffer()
 {
@@ -167,6 +237,7 @@ static void CreateVertexBuffer()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
+
 
 static void CreateIndexBuffer()
 {
@@ -200,6 +271,7 @@ static void CreateIndexBuffer()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 }
+*/
 
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
 {
@@ -320,10 +392,10 @@ int main(int argc, char** argv)
     glFrontFace(GL_CW);
     glCullFace(GL_BACK);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe mode
 
-    CreateVertexBuffer();
-    CreateIndexBuffer();
+    //CreateVertexBuffer();
+    //CreateIndexBuffer();
 
     CompileShaders();
 
