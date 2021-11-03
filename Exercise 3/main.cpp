df7e1774f15459fc6c4ba93dc9f7460e2a967b89
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2021-11-03 18:38:22 -0300

    Subdivide icosahedron by 4
    As required by the exercise 3.

--- Exercise 3/main.cpp
+++ Exercise 3/main.cpp
@@ -308,7 +308,8 @@ int main(int argc, char** argv)
     table.genVBO(&VBO[0]);
     table.genIBO(&IBO[0]);
 
-    Mesh icosahedron = createSubdividedIcosahedron(3, color_yellow);
+    // icosahedron subdivided by 4, as required by the exercise
+    Mesh icosahedron = createSubdividedIcosahedron(4, color_yellow);
     Transformation t1 = Transformation();
     t1.setScale({ 0.3f, 0.3f, 0.3f });  // scale icosahedron down
     t1.setTranslation({ 0.5f, 0.85f, 0.0f }); // moves icosahedron to the top of the table
